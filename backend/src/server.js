da4541fb806398f16c803268d05962286c2c6f54
Suhel <jahidulsuhel@gmail.com>
2025-05-20 22:51:58 +0600

    v 0.0.1

--- backend/src/server.js
+++ backend/src/server.js
@@ -18,7 +18,7 @@ app.use(cors({
 }));
 
 app.use("/api/auth", authRoute)
-app.use("/api/message", messageRoutes)
+app.use("/api/messages", messageRoutes)
 
 const PORT = process.env.PORT;
 app.listen(PORT, ()=>{
