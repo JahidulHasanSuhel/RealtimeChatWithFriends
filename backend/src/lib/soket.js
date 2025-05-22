77a017059bdd60228cfbb9b632b76bb3e2d9a5f8
Suhel <jahidulsuhel@gmail.com>
2025-05-22 23:01:26 +0600

    v 1.0.0

--- backend/src/lib/soket.js
+++ backend/src/lib/soket.js
@@ -21,16 +21,21 @@ io.on("connection", (socket) => {
     console.log("A user connected", socket.id);
 
     const userId = socket.handshake.query.userId;
-    if(userId) userSocketMap[userId] = socket.id;
+    if(userId){
+        userSocketMap[userId] = socket.id;
+        socket.userId = userId;
+    } 
 
     //io.emit() is used to send events to all the connected clients
     io.emit("getOnlineUsers", Object.keys(userSocketMap));
 
-    socket.on("disconnect", ()=>{
-        console.log("A user disconnected", socket.id);
-        delete userSocketMap [userId];
-        io.emit("getOnlineUsers", Object.keys(userSocketMap));
-    })
+    socket.on("disconnect", () => {
+      console.log("User disconnected:", socket.id);
+      if (socket.userId) {
+        delete userSocketMap[socket.userId];
+      }
+      io.emit("getOnlineUsers", Object.keys(userSocketMap));
+    });      
 });
 
 export {io, app, server};
